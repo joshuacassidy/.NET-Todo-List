0067d1412a22ef92a21d8aedb45e0caf18ba7b70
joshuacassidy <joshcassidy79@gmail.com>
2020-01-27 10:19:35 +0000

    Initial commit

---
+++ TodoList/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TodoList.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int TodoId { get; set; }
+        public bool Completed { get; set; }
+        public string Text { get; set; }
+        
+    }
+}
---
+++ TodoList/Models/TodoContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TodoList.Models;
+
+namespace TodoList_Demo.Models
+{
+
+    public class TodoContext : DbContext
+    {
+        public TodoContext() : base("TodoContext")
+        {
+
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+    }
+
+}
